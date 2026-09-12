56ea966ace9002508b89e96a8e950da6e7db1d2e
Musaib Rashid Bhat <168916820+MusaibRasheed@users.noreply.github.com>
2026-09-12 09:37:22 +0000

    update middleware

--- ai-soc-assistant/backend/main.py
+++ ai-soc-assistant/backend/main.py
@@ -35,12 +35,12 @@ async def validation_exception_handler(request: Request, exc: RequestValidationE
 
 app.add_middleware(
     CORSMiddleware,
-   allow_origins=[
-    "https://al-assisted-threat-detection-incide.vercel.app",
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
-    "http://localhost:3000",
-]
+    allow_origins=[
+        "https://al-assisted-threat-detection-incide.vercel.app",
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "http://localhost:3000",
+    ],
     allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
